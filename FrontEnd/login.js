754c40cfff914c661e59ee7546575f3a7d7a5613
John <johndoe@example.com>
2024-03-19 11:27:16 +0100

    prejet_presque_fini

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -15,24 +15,24 @@ function login(username, password) {
             let reponseServeur = response.json()
             if (response.status === 200) {
                 reponseServeur.then(data => {
-                    console.log(data);
-                    localStorage.setItem("token", JSON.stringify(data.token));
-                    window.location.href="./index.html";                
+                    console.log(data)
+                    localStorage.setItem("token", JSON.stringify(data.token))
+                    window.location.href="./index.html"               
                 })
             } else {                
-               let messageErreur = document.getElementById("error_message");
-               messageErreur.innerText = "Erreur dans l’identifiant ou le mot de passe";                   
+               let messageErreur = document.getElementById("error_message")
+               messageErreur.innerText = "Erreur dans l’identifiant ou le mot de passe"                   
             }
         })
         
         
-};
+}
 
 let token = localStorage.getItem("token");
 
 document.getElementById("login_form").addEventListener("submit", function (event) {
-    event.preventDefault();
-    let username = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    login(username, password);
+    event.preventDefault()
+    let username = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    login(username, password)
 })
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,3 +1,5 @@
+// import { closeModal } from "./app.js";
+
 // partie 1: affichage dynamique des travaux
 function integrerWorks(article) {
     const gallery = document.querySelector(".gallery");
@@ -12,33 +14,20 @@ function integrerWorks(article) {
     figure.appendChild(caption);
 };
 
-// function getWorks() {
-//     fetch('http://localhost:5678/api/works')
-//         .then(response => response.json())
-//         .then(works => {
-//             if (Array.isArray(works)) {
-//                 localStorage.setItem("works", JSON.stringify(works));
-//             } else {
-//                 console.error('La réponse JSON n\'est pas un tableau.');
-//             }
-//         })
-//         .catch(error => console.error('Erreur lors de la requête fetch :', error));
-// };
-
 async function getWorks() {
     try {
         const response = await fetch('http://localhost:5678/api/works');
         const works = await response.json();
 
         if (Array.isArray(works)) {
-            localStorage.setItem("works", JSON.stringify(works));
+            4
         } else {
             console.error('La réponse JSON n\'est pas un tableau.');
         }
     } catch (error) {
         console.error('Erreur lors de la requête fetch :', error);
     }
-}
+};
 
 getWorks();
 
@@ -46,34 +35,36 @@ let worksStorageString = localStorage.getItem("works");
 let works = JSON.parse(worksStorageString);
 
 function afficherWorks() {
-    for (const photo of works) {      
+    for (const photo of works) {
         integrerWorks(photo);
-    };
+    }
 };
 
 afficherWorks();
 
-// partie 2: filtres par catégorie
+// partie 1.2: filtres par catégorie
 
 function afficherWorksFiltered(category) {
     for (const photo of works) {
-        if (photo.categoryId === category.id) {            
+        if (photo.categoryId === category.id) {
             integrerWorks(photo);
         }
-    };
+    }
 };
 
-function getCategories() {
-    fetch('http://localhost:5678/api/categories')
-        .then(response => response.json())
-        .then(categories => {
-            if (Array.isArray(categories)) {
-                localStorage.setItem("categories", JSON.stringify(categories));
-            } else {
-                console.error('La réponse JSON n\'est pas un tableau.');
-            }
-        })
-        .catch(error => console.error('Erreur lors de la requête fetch :', error));
+async function getCategories() {
+    try {
+        const response = await fetch('http://localhost:5678/api/categories');
+        const categories = await response.json();
+
+        if (Array.isArray(categories)) {
+            localStorage.setItem("categories", JSON.stringify(categories));
+        } else {
+            console.error('La réponse JSON n\'est pas un tableau.');
+        }
+    } catch {
+        (error => console.error('Erreur lors de la requête fetch :', error));
+    }
 };
 
 getCategories();
@@ -86,13 +77,13 @@ function integrerBoutons(article) {
     const button = document.createElement("button");
     button.innerText = article.name;
     button.setAttribute("id", article.id);
-    button.addEventListener("click", ()=> {
+    button.addEventListener("click", () => {
         cleanWorks();
         afficherWorksFiltered(article);
-        cleanButtons();  
-        document.getElementById("boutton_tous").classList.remove("filter_clicked");   
+        cleanButtons();
+        document.getElementById("boutton_tous").classList.remove("filter_clicked");
         button.classList.add("filter_clicked");
-    })
+    });
 
     divButtons.appendChild(button);
 };
@@ -100,14 +91,14 @@ function integrerBoutons(article) {
 function cleanWorks() {
     const gallery = document.querySelector(".gallery");
     gallery.innerHTML = "";
-}
+};
 
 function cleanButtons() {
     let buttons = document.querySelectorAll(".buttons button");
-    buttons.forEach(function(button){
-        button.classList.remove("filter_clicked"); 
-    }) 
-}
+    buttons.forEach(function (button) {
+        button.classList.remove("filter_clicked");
+    });
+};
 
 const buttonTous = document.getElementById("boutton_tous");
 buttonTous.addEventListener("click", () => {
@@ -115,29 +106,31 @@ buttonTous.addEventListener("click", () => {
     afficherWorks();
     cleanButtons();
     buttonTous.classList.add("filter_clicked");
-})
+});
 
-for (i = 0; i < 3; i++) {
-    integrerBoutons(categories[i]);    
-}
+for (let i = 0; i < 3; i++) {
+    integrerBoutons(categories[i]);
+};
 
 
-// mode edition
+// partie 2 : login + app.js
 
-document.addEventListener("DOMContentLoaded", function() {
-    if (localStorage.getItem('token') !== null){
+document.addEventListener("DOMContentLoaded", function () {
+    if (localStorage.getItem('token') !== null) {
         document.getElementById("buttons_categories").style.display = "none";
         document.getElementById("m_edition").style.display = "flex";
         document.getElementById("modal_edition").style.display = "block";
         document.getElementById("menu_login").style.display = "none";
         document.getElementById("menu_logout").style.display = "block";
     }
-})
+});
 
 const boutonLogout = document.getElementById("menu_logout");
 boutonLogout.addEventListener('click', () => {
     localStorage.removeItem('token');
-})
+});
+
+// partie 3: modale
 
 function integrerMiniWorks(article) {
     const miniGallery = document.querySelector(".mini-gallery");
@@ -148,7 +141,7 @@ function integrerMiniWorks(article) {
     trashPic.src = "assets/icons/trash.png";
     miniFig.classList.add("mini-fig");
     miniPic.classList.add("mini-pic");
-    trashPic.classList.add("trash-pic");   
+    trashPic.classList.add("trash-pic");
     trashPic.addEventListener("click", (event) => {
         event.preventDefault();
         supprimerWorks(article.id);
@@ -161,23 +154,23 @@ function integrerMiniWorks(article) {
 };
 
 function afficherMiniWorks() {
-    for (const photo of works) {        
+    for (const photo of works) {
         integrerMiniWorks(photo);
     };
 };
 
-afficherMiniWorks()
+afficherMiniWorks();
 
-function supprimerWorks(idImage) {
-    // fetch delete
-    fetch(`http://localhost:5678/api/works/${idImage}`, {
-        method: "DELETE",
-        headers: {
-            "Authorization" : `Bearer ${JSON.parse(localStorage.getItem('token'))}`
-        }        
-    })
-    .then(response => {
-        if(response.ok) {
+async function supprimerWorks(idImage) {
+    try {
+        const response = await fetch(`http://localhost:5678/api/works/${idImage}`, {
+            method: "DELETE",
+            headers: {
+                "Authorization": `Bearer ${JSON.parse(localStorage.getItem('token'))}`
+            }
+        });
+        
+        if (response.ok) {
             let localWorks = JSON.parse(localStorage.getItem("works"));
             localWorks = localWorks.filter(work => work.id !== idImage);
             localStorage.setItem("works", JSON.stringify(localWorks));
@@ -188,35 +181,36 @@ function supprimerWorks(idImage) {
             afficherWorks();
             afficherMiniWorks();
         }
-    })
-    .catch(error => console.error("Erreur: ", error))    
-}
+    } catch (error) {
+        console.error("Erreur: ", error);
+    }
+};
 
 function cleanMiniWorks() {
     const gallery = document.querySelector(".mini-gallery");
     gallery.innerHTML = "";
-}
+};
 
 // deuxième modale
-const boutonAjouterPhoto = document.getElementById('bouton_ajouter_photo')
+const boutonAjouterPhoto = document.getElementById('bouton_ajouter_photo');
 boutonAjouterPhoto.addEventListener('click', () => {
-    const divGalleryPhoto = document.getElementById('galerie_photo')
-    divGalleryPhoto.style.display = 'none' 
-    const divAjoutPhoto = document.getElementById('ajout_photo')
-    divAjoutPhoto.style.display = 'block' 
-    const modalReturn = document.querySelector('.modal-return')
-    modalReturn.style.display = 'flex'
-    const modalXmarkDiv = document.querySelector('.modal-xmark')
-    modalXmarkDiv.style.justifyContent = 'space-between'
+    const divGalleryPhoto = document.getElementById('galerie_photo');
+    divGalleryPhoto.style.display = 'none';
+    const divAjoutPhoto = document.getElementById('ajout_photo');
+    divAjoutPhoto.style.display = 'block';
+    const modalReturn = document.querySelector('.modal-return');
+    modalReturn.style.display = 'flex';
+    const modalXmarkDiv = document.querySelector('.modal-xmark');
+    modalXmarkDiv.style.justifyContent = 'space-between';
 
     // selection fichier
-    document.getElementById('add-file').addEventListener('change', function(event) {
+    document.getElementById('add-file').addEventListener('change', function (event) {
         const file = event.target.files[0];
         if (file) {
             const reader = new FileReader();
-            reader.onload = function(e) {
+            reader.onload = function (e) {
                 document.getElementById('preview-image').src = e.target.result;
-    
+
                 // chacher le reste de la div
                 document.getElementById('add-file').style.display = 'none';
                 document.querySelector('label[for="add-file"]').style.display = 'none';
@@ -225,51 +219,94 @@ boutonAjouterPhoto.addEventListener('click', () => {
             reader.readAsDataURL(file);
         }
     });
-})
+});
 
 // inverser
-const boutonReturn = document.querySelector('.modal-return')
+const boutonReturn = document.querySelector('.modal-return');
 boutonReturn.addEventListener('click', () => {
-    divGalleryPhoto = document.getElementById('galerie_photo')
-    divGalleryPhoto.style.display = 'block' 
-    divAjoutPhoto = document.getElementById('ajout_photo')
-    divAjoutPhoto.style.display = 'none' 
-    modalReturn = document.querySelector('.modal-return')
-    modalReturn.style.display = 'none'
-    modalXmarkDiv = document.querySelector('.modal-xmark')
-    modalXmarkDiv.style.justifyContent = 'flex-end'
-
-    // Reset ajout-pic à l'état d'avant la selection de fichier
-    const previewImage = document.getElementById('preview-image');
-    previewImage.src = "assets/icons/grey_pic.png";
-    document.getElementById('add-file').style.display = 'block';
-    document.querySelector('label[for="add-file"]').style.display = 'block';
-    document.querySelector('.text-10px').style.display = 'block';
-})
+    divGalleryPhoto = document.getElementById('galerie_photo');
+    divGalleryPhoto.style.display = 'block';
+    divAjoutPhoto = document.getElementById('ajout_photo');
+    divAjoutPhoto.style.display = 'none';
+    modalReturn = document.querySelector('.modal-return');
+    modalReturn.style.display = 'none';
+    modalXmarkDiv = document.querySelector('.modal-xmark');
+    modalXmarkDiv.style.justifyContent = 'flex-end';
+});
 
 function categoriesAjoutPhoto(cat) {
-    const addCategorie = document.getElementById('add-categorie')
-    const optionCategorie = document.createElement('option')
-    optionCategorie.innerText = cat.name
+    const addCategorie = document.getElementById('add-categorie');
+    const optionCategorie = document.createElement('option');
+    optionCategorie.innerText = cat.name;
+    optionCategorie.value = cat.id;
 
-    addCategorie.appendChild(optionCategorie)
+    addCategorie.appendChild(optionCategorie);
 }
 
-for (i = 0; i < 3; i++) {
-    categoriesAjoutPhoto(categories[i]);    
+for (let i = 0; i < 3; i++) {
+    categoriesAjoutPhoto(categories[i]);
 }
 
-// const addTitle = document.getElementById('add-title')
-// addTitle.addEventListener('change', () => {
-//     console.log(addTitle.value)
-//     if (addTitle.value !== null || addTitle.value !== "" || addTitle.value.length  !== 0){
-//         let submitButtonM = document.getElementById('submit-button-modale2')
-//         submitButtonM.removeAttribute("disabled")
-//         submitButtonM.style.backgroundColor = '#1D6154'
-//     } else {
-//         console.log("else part")
-//         document.getElementById('submit-button-modale2').addAttribute("disabled")
-//         submitButtonM.style.backgroundColor = '#A7A7A7'
-//     }
+const form = document.getElementById('form-new-work');
 
-// })
+form.addEventListener('input', function () {
+    const addTitle = document.getElementById('add-title');
+    const addFile = document.getElementById('add-file');
+    const addCategorie = document.getElementById('add-categorie');
+    
+    const isTitleFilled = addTitle.value.trim() !== '';
+    const isFileSelected = addFile.files.length > 0;
+    const isCategorySelected = addCategorie.value.trim() !== '';
+
+    if (isTitleFilled && isFileSelected && isCategorySelected) {
+        console.log("Tous les champs sont remplis !");
+        let submitButtonM = document.getElementById('submit-button-modale2');
+        submitButtonM.style.backgroundColor = '#1D6154';
+        submitButtonM.style.cursor = 'pointer';
+        submitButtonM.removeAttribute("disabled");
+    }
+});
+
+// formulaire
+form.addEventListener('submit', async function(event) {
+    event.preventDefault(); 
+
+    const formData = new FormData();
+
+    formData.append("title", document.getElementById('add-title').value)
+    formData.append("image", document.getElementById('add-file').files[0])
+    formData.append("category", document.getElementById('add-categorie').value)
+    console.log(formData);
+    console.log(`${document.getElementById('add-title').value}`)
+    console.log(`${document.getElementById('add-file').files[0]}`)
+    console.log(`${document.getElementById('add-categorie').value}`)
+
+    try {
+        const response = await fetch('http://localhost:5678/api/works/', {
+            method: 'POST',
+            body: formData,
+            headers: {
+                "Authorization": `Bearer ${JSON.parse(localStorage.getItem('token'))}`
+            }
+        });
+
+        if (response.ok) {
+            console.log('Données envoyées avec succès !');
+            const data = await response.json();
+            console.log(data);
+            integrerWorks(data);
+            integrerMiniWorks(data);
+            works.push(data);
+            console.log(works);
+            localStorage.setItem("works", JSON.stringify(works));
+            // document.getElementById('modal1').style.display = 'none';
+            // closeModal();
+            
+
+        } else {
+            console.error('Échec de l\'envoi des données.');
+        }
+    } catch (error) {
+        console.error('Erreur lors de l\'envoi des données :', error);
+    }
+});
