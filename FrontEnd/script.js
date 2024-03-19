9c789d704ee90417f93ebf83843ca9bda8152929
John <johndoe@example.com>
2024-03-19 17:15:24 +0100

    works

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -18,6 +18,7 @@ async function getWorks() {
     try {
         const response = await fetch('http://localhost:5678/api/works');
         const works = await response.json();
+        console.log(works);
 
         if (Array.isArray(works)) {
             4
